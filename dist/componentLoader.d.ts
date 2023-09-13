a4d332b2e11af60affd923e4d0dc888f24807866
zerocho <zerohch0@gmail.com>
2023-09-13 14:57:22 +0900

    Remove: outdir

--- dist/componentLoader.d.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentLoader } from 'adminjs';
-export declare const componentLoader: ComponentLoader;
-export declare const Components: {
-    Dashboard: string;
-    DisplayImage: string;
-    IdQuerySaver: string;
-    ExportCsv: string;
-};
--- dist/componentLoader.js
+++
@@ -1,9 +0,0 @@
-import { ComponentLoader } from 'adminjs';
-export const componentLoader = new ComponentLoader();
-export const Components = {
-    Dashboard: componentLoader.add('Dashboard', './components/Dashboard'),
-    DisplayImage: componentLoader.add('DisplayImage', './components/DisplayImage'),
-    IdQuerySaver: componentLoader.add('IdQuerySaver', './components/IdQuerySaver'),
-    ExportCsv: componentLoader.add('ExportCsv', './components/ExportCsv'),
-};
-//# sourceMappingURL=componentLoader.js.map
