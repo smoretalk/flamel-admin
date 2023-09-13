2d9fa9d9a4a60b5fa22f2b729316b8ff3c58d026
zerocho <zerohch0@gmail.com>
2023-09-13 15:15:20 +0900

    Update: componentLoader path resolver

--- componentLoader.js
+++ componentLoader.js
@@ -1,9 +1,13 @@
 import { ComponentLoader } from 'adminjs';
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export const componentLoader = new ComponentLoader();
 export const Components = {
-    Dashboard: componentLoader.add('Dashboard', './components/Dashboard'),
-    DisplayImage: componentLoader.add('DisplayImage', './components/DisplayImage'),
-    IdQuerySaver: componentLoader.add('IdQuerySaver', './components/IdQuerySaver'),
-    ExportCsv: componentLoader.add('ExportCsv', './components/ExportCsv'),
+    Dashboard: componentLoader.add('Dashboard', path.resolve(__dirname, 'components', 'Dashboard')),
+    DisplayImage: componentLoader.add('DisplayImage', path.resolve(__dirname, 'components', 'DisplayImage')),
+    IdQuerySaver: componentLoader.add('IdQuerySaver', path.resolve(__dirname, 'components', 'IdQuerySaver')),
+    ExportCsv: componentLoader.add('ExportCsv', path.resolve(__dirname, 'components', 'ExportCsv')),
 };
 //# sourceMappingURL=componentLoader.js.map
--- componentLoader.ts
+++ componentLoader.ts
@@ -1,13 +1,17 @@
 import { ComponentLoader } from 'adminjs'
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const componentLoader = new ComponentLoader()
 
 export const Components = {
-  Dashboard: componentLoader.add('Dashboard', './components/Dashboard'),
+  Dashboard: componentLoader.add('Dashboard', path.resolve(__dirname, 'components', 'Dashboard')),
   DisplayImage: componentLoader.add(
       'DisplayImage',
-      './components/DisplayImage',
+    path.resolve(__dirname, 'components', 'DisplayImage'),
   ),
-  IdQuerySaver: componentLoader.add('IdQuerySaver', './components/IdQuerySaver'),
-  ExportCsv: componentLoader.add('ExportCsv', './components/ExportCsv'),
+  IdQuerySaver: componentLoader.add('IdQuerySaver', path.resolve(__dirname, 'components', 'IdQuerySaver')),
+  ExportCsv: componentLoader.add('ExportCsv', path.resolve(__dirname, 'components', 'ExportCsv')),
 }
