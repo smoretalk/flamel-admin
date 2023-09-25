5e4e8d8ace9f1ee57b9ab9b5224e4e4768106d2c
zerocho <zerohch0@gmail.com>
2023-09-25 12:22:20 +0900

    Fix: missing files

---
+++ components/DisplayNestedImage.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import type { ShowPropertyProps } from 'adminjs';
+declare const DisplayNestedImage: React.FC<ShowPropertyProps & {
+    where: 'show' | 'list';
+}>;
+export default DisplayNestedImage;
---
+++ components/List.js
@@ -0,0 +1,41 @@
+import { Box, Pagination, Text } from '@adminjs/design-system';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+export const REFRESH_KEY = 'refresh';
+import { allowOverride, useRecords, useSelectedRecords, getActionElementCss, RecordsTable } from 'adminjs';
+const List = ({ resource, setTag }) => {
+    const { records, loading, direction, sortBy, page, total, fetchData, perPage, } = useRecords(resource.id);
+    console.log('records', records);
+    const { selectedRecords, handleSelect, handleSelectAll, setSelectedRecords, } = useSelectedRecords(records);
+    console.log('selectedRecords', selectedRecords);
+    const location = useLocation();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (setTag) {
+            setTag(total.toString());
+        }
+    }, [total]);
+    useEffect(() => {
+        setSelectedRecords([]);
+    }, [resource.id]);
+    useEffect(() => {
+        const search = new URLSearchParams(location.search);
+        if (search.get(REFRESH_KEY)) {
+            setSelectedRecords([]);
+        }
+    }, [location.search]);
+    const handleActionPerformed = () => fetchData();
+    const handlePaginationChange = (pageNumber) => {
+        const search = new URLSearchParams(location.search);
+        search.set('page', pageNumber.toString());
+        navigate({ search: search.toString() });
+    };
+    const contentTag = getActionElementCss(resource.id, 'list', 'table-wrapper');
+    return (React.createElement(Box, { variant: "container", "data-css": contentTag },
+        React.createElement(RecordsTable, { resource: resource, records: records, actionPerformed: handleActionPerformed, onSelect: handleSelect, onSelectAll: handleSelectAll, selectedRecords: selectedRecords, direction: direction, sortBy: sortBy, isLoading: loading }),
+        React.createElement(Text, { mt: "xl", textAlign: "center" },
+            React.createElement(Pagination, { page: page, perPage: perPage, total: total, onChange: handlePaginationChange }))));
+};
+const OverridableList = allowOverride(List, 'DefaultListAction');
+export default OverridableList;
+//# sourceMappingURL=List.js.map
