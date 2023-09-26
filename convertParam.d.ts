8c0fe1aea4c3ee472e1f9cd15f08c5149731cd77
zerocho <zerohch0@gmail.com>
2023-09-26 14:37:05 +0900

    Fix: missing convertParam.js

---
+++ convertParam.d.ts
@@ -0,0 +1,4 @@
+import { Property } from "./customProperty.js";
+export declare const isNumeric: (value: any) => boolean;
+export declare const safeParseNumber: (value: any) => any;
+export declare const convertParam: (property: Property, fields: any, value: any, nested?: boolean) => any;
---
+++ convertParam.js
@@ -0,0 +1,42 @@
+export const isNumeric = (value) => {
+    const stringValue = (String(value)).replace(/,/g, '.');
+    if (isNaN(parseFloat(stringValue)))
+        return false;
+    return isFinite(Number(stringValue));
+};
+export const safeParseNumber = (value) => {
+    if (isNumeric(value))
+        return Number(value);
+    return value;
+};
+export const convertParam = (property, fields, value, nested = false) => {
+    const type = property.type();
+    if (type === 'mixed')
+        return value;
+    if (type === 'number') {
+        return safeParseNumber(value);
+    }
+    if (type === 'boolean') {
+        return value === true || value === 'true';
+    }
+    if (type === 'reference') {
+        const foreignColumn = fields.find((field) => field.name === property.foreignColumnName());
+        if (!foreignColumn)
+            return value;
+        if (value === undefined || value === null)
+            return value;
+        const foreignColumnType = foreignColumn.type;
+        if (nested) {
+            if (value === undefined || value === null)
+                return { disconnect: true };
+            if (foreignColumnType === 'String')
+                return { connect: { id: String(value) } };
+            return { connect: { id: safeParseNumber(value) } };
+        }
+        if (foreignColumnType === 'String')
+            return String(value);
+        return safeParseNumber(value);
+    }
+    return value;
+};
+//# sourceMappingURL=convertParam.js.map
