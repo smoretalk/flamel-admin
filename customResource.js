2434ecfe0a77991ebb4ad9488b0e780fe663cb30
zerocho <zerohch0@gmail.com>
2023-09-26 12:57:09 +0900

    Update: 조회 시 중첩된 객체에서 id 추출해서 교체

--- customResource.js
+++ customResource.js
@@ -1,6 +1,217 @@
-import { Resource } from '@adminjs/prisma';
-import { BaseRecord } from 'adminjs';
-export class CustomResource extends Resource {
+import { BaseRecord, BaseResource, flat } from 'adminjs';
+import { convertFilter, convertParam, getEnums } from "@adminjs/prisma";
+export const lowerCase = (name) => name.substring(0, 1).toLowerCase() + name.substring(1);
+export class CustomResource extends BaseResource {
+    model;
+    client;
+    enums;
+    manager;
+    propertiesObject;
+    include;
+    depModels;
+    depModelsObject;
+    constructor(args) {
+        super(args);
+        const { model, client, clientModule, include, depModels } = args;
+        this.model = model;
+        this.client = client;
+        this.enums = getEnums(clientModule);
+        this.manager = this.client[lowerCase(model.name)];
+        this.propertiesObject = this.prepareProperties();
+        this.include = include || {};
+        this.depModels = depModels || [];
+        this.depModelsObject = depModels?.map((model) => this.prepareDepModelProperties(model));
+        this.depModelsObject?.forEach((depModels) => {
+            this.propertiesObject = {
+                ...depModels,
+                ...this.propertiesObject,
+            };
+        });
+    }
+    databaseName() {
+        return 'prisma';
+    }
+    databaseType() {
+        return this.client._engineConfig?.activeProvider ?? 'database';
+    }
+    id() {
+        return this.model.name;
+    }
+    properties() {
+        return [...Object.values(this.propertiesObject)];
+    }
+    property(path) {
+        return this.propertiesObject[path] ?? null;
+    }
+    build(params) {
+        return new BaseRecord(flat.unflatten(params), this);
+    }
+    async count(filter) {
+        return this.manager.count({ where: convertFilter(this.model.fields, filter) });
+    }
+    async find(filter, params = {}) {
+        console.log('find called', this.include);
+        const { limit = 10, offset = 0, sort = {} } = params;
+        const { direction, sortBy } = sort;
+        const where = convertFilter(this.model.fields, filter);
+        console.log('where', where);
+        const results = await this.manager.findMany({
+            where,
+            skip: offset,
+            take: limit,
+            include: this.include,
+            orderBy: {
+                [sortBy]: direction,
+            },
+        });
+        return results.map((result) => new BaseRecord(this.prepareReturnValues(result), this));
+    }
+    async findOne(id) {
+        console.log('findOne called');
+        const idProperty = this.properties().find((property) => property.isId());
+        if (!idProperty)
+            return null;
+        const result = await this.manager.findUnique({
+            where: {
+                [idProperty.path()]: convertParam(idProperty, this.model.fields, id),
+            },
+            include: this.include,
+        });
+        if (!result)
+            return null;
+        return new BaseRecord(this.prepareReturnValues(result), this);
+    }
+    async findMany(ids) {
+        console.log('findMany called');
+        const idProperty = this.properties().find((property) => property.isId());
+        if (!idProperty)
+            return [];
+        const results = await this.manager.findMany({
+            where: {
+                [idProperty.path()]: {
+                    in: ids.map((id) => convertParam(idProperty, this.model.fields, id)),
+                },
+            },
+            include: this.include,
+        });
+        return results.map((result) => new BaseRecord(this.prepareReturnValues(result), this));
+    }
+    async create(params) {
+        const preparedParams = this.prepareParams(params);
+        const result = await this.manager.create({ data: preparedParams });
+        return this.prepareReturnValues(result);
+    }
+    async update(pk, params = {}) {
+        console.log('update');
+        const idProperty = this.properties().find((property) => property.isId());
+        if (!idProperty)
+            return {};
+        const preparedParams = this.prepareParams(params);
+        console.log('preparedParams', preparedParams, idProperty.path(), convertParam(idProperty, this.model.fields, pk));
+        const result = await this.manager.update({
+            where: {
+                [idProperty.path()]: convertParam(idProperty, this.model.fields, pk),
+            },
+            data: preparedParams,
+        });
+        console.log('udpate result', result);
+        return this.prepareReturnValues(result);
+    }
+    async delete(id) {
+        const idProperty = this.properties().find((property) => property.isId());
+        if (!idProperty)
+            return;
+        await this.manager.delete({
+            where: {
+                [idProperty.path()]: convertParam(idProperty, this.model.fields, id),
+            },
+        });
+    }
+    static isAdapterFor(args) {
+        const { model, client } = args;
+        return !!model?.name && !!model?.fields.length && !!client?.[lowerCase(model.name)];
+    }
+    prepareProperties() {
+        const { fields = [] } = this.model;
+        return fields.reduce((memo, field) => {
+            if (field.isReadOnly) {
+                return memo;
+            }
+            const property = new Property(field, Object.keys(memo).length, this.enums);
+            memo[property.path()] = property;
+            return memo;
+        }, {});
+    }
+    prepareDepModelProperties(model) {
+        const { fields = [], name } = model;
+        return fields.reduce((memo, field) => {
+            if (field.isReadOnly) {
+                return memo;
+            }
+            const property = new Property(field, Object.keys(memo).length, this.enums);
+            property.depModel = name;
+            property.depModelObject = model;
+            memo[`${name}.${property.path()}`] = property;
+            return memo;
+        }, {});
+    }
+    prepareParams(params) {
+        console.log('prepareParams', params);
+        const preparedParams = {};
+        for (const property of this.properties()) {
+            const param = flat.get(params, property.path());
+            const key = property.path();
+            if (param === undefined)
+                continue;
+            const type = property.type();
+            const foreignColumnName = property.foreignColumnName();
+            if (type === 'reference' && foreignColumnName) {
+                preparedParams[foreignColumnName] = convertParam(property, this.model.fields, param);
+                continue;
+            }
+            if (type === 'reference' && typeof param === 'object' && !Array.isArray(param)) {
+                continue;
+            }
+            if (property.isArray()) {
+                preparedParams[key] = param ? param.map((p) => convertParam(property, this.model.fields, p)) : param;
+            }
+            else {
+                preparedParams[key] = convertParam(property, this.model.fields, param);
+            }
+        }
+        return preparedParams;
+    }
+    isNonArrayObject(target) {
+        return typeof target === 'object' && !Array.isArray(target);
+    }
+    prepareReturnValues(params) {
+        const preparedValues = {};
+        for (const property of this.properties()) {
+            const param = flat.get(params, property.path());
+            const key = property.path();
+            if (!param && property.depModel && params?.[property.depModel]) {
+                preparedValues[`${property.depModel}.${key}`] = params?.[property.depModel][key];
+                if (property.type() === 'reference' && property.depModelObject.fields && this.isNonArrayObject(params?.[property.depModel][key])) {
+                    const idField = property.depModelObject.fields.find((field) => field.isId);
+                    preparedValues[`${property.depModel}.${key}`] = params?.[property.depModel][key]?.[idField];
+                }
+                continue;
+            }
+            if (param !== undefined && property.type() !== 'reference') {
+                preparedValues[key] = param;
+                continue;
+            }
+            if (Array.isArray(param)) {
+                preparedValues[key] = param;
+            }
+            const foreignColumnName = property.foreignColumnName();
+            if (!foreignColumnName)
+                continue;
+            preparedValues[key] = params[foreignColumnName];
+        }
+        console.log('preparedValues', preparedValues);
+        return preparedValues;
+    }
     titleField() {
         return this.decorate().titleProperty().name();
     }
