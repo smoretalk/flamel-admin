83209ea4b13bbd2a5dd3777ecd9e76a7280de99d
zerocho <zerohch0@gmail.com>
2024-04-16 11:42:48 +0900

    fix: ImageIdLink

--- components/ImageIdLink.tsx
+++ components/ImageIdLink.tsx
@@ -7,25 +7,22 @@ const ImageIdLink: React.FC<ShowPropertyProps & { where: 'show' | 'list' }> = (
 ) => {
   const [src, setSrc] = useState(props.record.params[props.property.props.linkProp])
 
+  console.log(props);
   const srcId = Number(src);
-  if (!Number.isNaN(srcId)) {
-    return (
-      <section style={{marginBottom: props.where === 'show' ? 24 : 0}}>
-        {props.where === 'show' && (
-          <Label>
-            {props.property.props.linkLabel}
-          </Label>
-        )}
-        {!Number.isNaN(srcId)
-          ? <Link variant="text"
-                  href={`/admin/resources/Image/records/${srcId}/show`}>{srcId}</Link>
-          : <span>{src}</span>
-        }
-      </section>
-    );
-  }
-
-
+  return (
+    <section style={{marginBottom: props.where === 'show' ? 24 : 0}}>
+      {props.where === 'show' && (
+        <Label variant="light">
+          {props.property.props.linkLabel}
+        </Label>
+      )}
+      {!Number.isNaN(srcId)
+        ? <Link variant="text"
+                href={`/admin/resources/Image/records/${srcId}/show`}>{srcId}</Link>
+        : <span>{src}</span>
+      }
+    </section>
+  );
 };
 
 export default ImageIdLink;
