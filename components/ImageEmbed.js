c6259af3d1eb1b313e622cd8920750899b37526b
zerocho <zerohch0@gmail.com>
2024-05-24 13:20:50 +0900

    update: 텍스트 임베딩 기능

--- components/ImageEmbed.js
+++ components/ImageEmbed.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { FilesetResolver, ImageEmbedder } from '@mediapipe/tasks-vision';
+import { FilesetResolver as TextFilesetResolver, TextEmbedder } from '@mediapipe/tasks-text';
 import axios from "axios";
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 export const ImageEmbed = () => {
     const [embedder, setEmbedder] = useState(null);
+    const [textEmbedder, setTextEmbedder] = useState(null);
     const [value, setValue] = useState('');
     useEffect(() => {
         async function main() {
-            const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
-            const imageEmbedder = await ImageEmbedder.createFromOptions(vision, {
-                baseOptions: {
-                    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/image_embedder/mobilenet_v3_small/float32/1/mobilenet_v3_small.tflite`
-                },
-            });
+            const [vision, text] = await Promise.all([
+                FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"),
+                TextFilesetResolver.forTextTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-text/wasm"),
+            ]);
+            const [imageEmbedder, textEmbedder] = await Promise.all([
+                ImageEmbedder.createFromOptions(vision, {
+                    baseOptions: {
+                        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/image_embedder/mobilenet_v3_small/float32/1/mobilenet_v3_small.tflite`
+                    },
+                }),
+                TextEmbedder.createFromModelPath(text, "https://storage.googleapis.com/mediapipe-models/text_embedder/universal_sentence_encoder/float32/1/universal_sentence_encoder.tflite"),
+            ]);
             setEmbedder(imageEmbedder);
+            setTextEmbedder(textEmbedder);
+            alert('모델 준비 완료');
         }
         main();
     }, []);
@@ -33,6 +43,19 @@ export const ImageEmbed = () => {
             });
         }
     };
+    const onTextStart = async () => {
+        const response = await axios.post('/admin/api/resources/Image/actions/list?filters.CollectionInfo.enabled=true&perPage=0');
+        const total = response.data.meta.total;
+        const response2 = await axios.post(`/admin/api/resources/Image/actions/list?filters.CollectionInfo.enabled=true&perPage=${total}`);
+        console.log(response2.data.records, textEmbedder);
+        for (const r of response2.data.records) {
+            const textEmbedderResult = textEmbedder.embed(r.params['CollectionInfo.stylePrompt'] || r.params['GenerationInfo.fullPrompt']);
+            console.log(r.id, textEmbedderResult);
+            await axios.patch(`/api/collections/${r.id}/textEmbed`, {
+                vector: JSON.stringify(textEmbedderResult.embeddings[0].floatEmbedding),
+            });
+        }
+    };
     const onChange = (e) => {
         setValue(e.target.value);
     };
@@ -48,11 +71,26 @@ export const ImageEmbed = () => {
         }).toSorted((a, b) => b.similar - a.similar);
         console.log(result);
     };
+    const onTextClick = async () => {
+        const response = await axios.get(`/api/collections/imageEmbed?take=1000`);
+        const target = response.data.find((v) => v.imageId.toString() === value);
+        const result = response.data.map((v) => {
+            const similar = TextEmbedder.cosineSimilarity({ floatEmbedding: JSON.parse(target.textEmbedding), headName: 'feature', headIndex: 0 }, { floatEmbedding: JSON.parse(v.textEmbedding), headName: 'feature', headIndex: 0 });
+            return {
+                imageId: v.imageId,
+                similar,
+            };
+        }).toSorted((a, b) => b.similar - a.similar);
+        console.log(result);
+    };
     return (React.createElement("div", null,
         React.createElement("img", { id: 'image', alt: '', width: '512', height: '512' }),
-        React.createElement("button", { onClick: onStart }, "\uC2DC\uC791"),
+        React.createElement("button", { onClick: onStart }, "\uC774\uBBF8\uC9C0 \uC784\uBCA0\uB529 \uC2DC\uC791"),
+        React.createElement("button", { onClick: onTextStart }, "\uD14D\uC2A4\uD2B8 \uC784\uBCA0\uB529 \uC2DC\uC791"),
+        React.createElement("br", null),
         React.createElement("input", { value: value, onChange: onChange }),
-        React.createElement("button", { onClick: onClick }, "\uC870\uD68C"),
+        React.createElement("button", { onClick: onClick }, "\uC774\uBBF8\uC9C0 \uC720\uC0AC\uB3C4 \uC870\uD68C"),
+        React.createElement("button", { onClick: onTextClick }, "\uD14D\uC2A4\uD2B8 \uC720\uC0AC\uB3C4 \uC870\uD68C"),
         React.createElement("div", null)));
 };
 export default ImageEmbed;
--- components/ImageEmbed.tsx
+++ components/ImageEmbed.tsx
@@ -1,26 +1,41 @@
 import React, {ChangeEventHandler, useEffect, useState} from "react";
 import { useTranslation } from 'adminjs';
 import { FilesetResolver, ImageEmbedder } from '@mediapipe/tasks-vision';
+import { FilesetResolver as TextFilesetResolver, TextEmbedder } from '@mediapipe/tasks-text';
 import axios from "axios";
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const ImageEmbed: React.FC = () => {
   const [embedder, setEmbedder] = useState<ImageEmbedder>(null);
+  const [textEmbedder, setTextEmbedder] = useState<TextEmbedder>(null);
   const [value, setValue] = useState('');
 
   useEffect(() => {
     async function main() {
-      const vision = await FilesetResolver.forVisionTasks(
-        // path/to/wasm/root
-        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-      );
-      const imageEmbedder = await ImageEmbedder.createFromOptions(vision, {
-        baseOptions: {
-          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/image_embedder/mobilenet_v3_small/float32/1/mobilenet_v3_small.tflite`
-        },
-      });
+      const [vision, text] = await Promise.all([
+        FilesetResolver.forVisionTasks(
+          // path/to/wasm/root
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+        ),
+        TextFilesetResolver.forTextTasks(
+          // path/to/wasm/root
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-text/wasm"
+        ),
+      ]);
+      const [imageEmbedder, textEmbedder] = await Promise.all([
+        ImageEmbedder.createFromOptions(vision, {
+          baseOptions: {
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/image_embedder/mobilenet_v3_small/float32/1/mobilenet_v3_small.tflite`
+          },
+        }),
+        TextEmbedder.createFromModelPath(text,
+          "https://storage.googleapis.com/mediapipe-models/text_embedder/universal_sentence_encoder/float32/1/universal_sentence_encoder.tflite"
+        ),
+      ]);
       setEmbedder(imageEmbedder);
+      setTextEmbedder(textEmbedder);
+      alert('모델 준비 완료');
     }
     main();
   }, []);
@@ -38,6 +53,20 @@ export const ImageEmbed: React.FC = () => {
       console.log(r.id, imageEmbedderResult);
       await axios.patch(`/api/collections/${r.id}/imageEmbed`, {
         vector: JSON.stringify(imageEmbedderResult.embeddings[0].floatEmbedding),
+      });
+    }
+  };
+
+  const onTextStart = async () => {
+    const response = await axios.post('/admin/api/resources/Image/actions/list?filters.CollectionInfo.enabled=true&perPage=0')
+    const total = response.data.meta.total;
+    const response2 = await axios.post<{ records: { id:number; params: { 'CollectionInfo.stylePrompt': string | null,'GenerationInfo.fullPrompt': string | null; Owner: number }}[] }>(`/admin/api/resources/Image/actions/list?filters.CollectionInfo.enabled=true&perPage=${total}`);
+    console.log(response2.data.records, textEmbedder);
+    for (const r of response2.data.records) {
+      const textEmbedderResult = textEmbedder.embed(r.params['CollectionInfo.stylePrompt'] || r.params['GenerationInfo.fullPrompt']);
+      console.log(r.id, textEmbedderResult);
+      await axios.patch(`/api/collections/${r.id}/textEmbed`, {
+        vector: JSON.stringify(textEmbedderResult.embeddings[0].floatEmbedding),
       });
     }
   };
@@ -62,12 +91,31 @@ export const ImageEmbed: React.FC = () => {
     console.log(result);
   }
 
+  const onTextClick = async () => {
+    const response = await axios.get<{ imageId: number; imageEmbedding: string; textEmbedding: string; }[]>(`/api/collections/imageEmbed?take=1000`);
+    const target = response.data.find((v) => v.imageId.toString() === value);
+    const result = response.data.map((v) => {
+      const similar = TextEmbedder.cosineSimilarity(
+        { floatEmbedding: JSON.parse(target.textEmbedding) as number[], headName: 'feature', headIndex: 0 },
+        { floatEmbedding: JSON.parse(v.textEmbedding) as number[], headName: 'feature', headIndex: 0 },
+      );
+      return {
+        imageId: v.imageId,
+        similar,
+      }
+    }).toSorted((a, b) => b.similar - a.similar);
+    console.log(result);
+  }
+
   return (
     <div>
       <img id='image' alt='' width='512' height='512' />
-      <button onClick={onStart}>시작</button>
+      <button onClick={onStart}>이미지 임베딩 시작</button>
+      <button onClick={onTextStart}>텍스트 임베딩 시작</button>
+      <br/>
       <input value={value} onChange={onChange} />
-      <button onClick={onClick}>조회</button>
+      <button onClick={onClick}>이미지 유사도 조회</button>
+      <button onClick={onTextClick}>텍스트 유사도 조회</button>
       <div>
 
       </div>
