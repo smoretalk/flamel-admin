7270fd0d25b30509e04cb3f7beab252435a0c804
zerocho <zerohch0@gmail.com>
2024-07-26 16:33:53 +0900

    update: add ownerId to coupon

--- components/CouponIssueSection.js
+++ components/CouponIssueSection.js
@@ -7,6 +7,7 @@ export default function CouponIssueSection({}) {
     const { translateMessage } = useTranslation();
     const [type, setType] = useState({ value: 'defaultCredit', label: '디폴트 크레딧' });
     const [date, setDate] = useState(null);
+    const [ownerId, setOwnerId] = useState(null);
     const [code, setCode] = useState('');
     const [credit, setCredit] = useState(0);
     function onChangeDate(date) {
@@ -27,15 +28,25 @@ export default function CouponIssueSection({}) {
     function onChangeCredit(e) {
         setCredit(parseInt(e.target.value, 10));
     }
+    function onChangeOwnerId(e) {
+        let value = parseInt(e.target.value, 10);
+        if (value) {
+            setOwnerId(value);
+        }
+        else {
+            setOwnerId(null);
+        }
+    }
     async function onSubmit(e) {
         e.preventDefault();
-        console.log('submitting', code, credit, date, type);
+        console.log('submitting', code, credit, date, type, ownerId);
         try {
             await axios.post('/api/coupons', {
                 code,
                 credit,
                 expiresAt: date,
                 type: type.value,
+                ownerId,
             });
             setCode('');
         }
@@ -65,6 +76,9 @@ export default function CouponIssueSection({}) {
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uD06C\uB808\uB527"),
                 React.createElement(Input, { onChange: onChangeCredit, type: "number", placeholder: "\uD06C\uB808\uB527 \uC218", value: credit })),
+            React.createElement(Box, null,
+                React.createElement(Label, null, "\uC0AC\uC6A9\uC790"),
+                React.createElement(Input, { onChange: onChangeOwnerId, type: "number", placeholder: "\uC9C0\uAE09 \uB300\uC0C1(\uBE48\uCE78\uC774\uBA74 \uB204\uAD6C\uB098 \uAC00\uB2A5)", value: ownerId })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uB9CC\uB8CC\uAE30\uD55C"),
                 React.createElement(DatePicker, { onChange: onChangeDate, propertyType: "date", value: date?.toString() })),
--- components/CouponIssueSection.tsx
+++ components/CouponIssueSection.tsx
@@ -20,6 +20,7 @@ export default function CouponIssueSection({}) {
   const { translateMessage } = useTranslation();
   const [type, setType] = useState<{ value: string; label: string }>({ value: 'defaultCredit', label: '디폴트 크레딧'});
   const [date, setDate] = useState<Date | null>(null);
+  const [ownerId, setOwnerId] = useState<number | null>(null);
   const [code, setCode] = useState<string>('');
   const [credit, setCredit] = useState<number>(0);
 
@@ -44,15 +45,25 @@ export default function CouponIssueSection({}) {
     setCredit(parseInt(e.target.value, 10));
   }
 
+  function onChangeOwnerId(e: ChangeEvent<HTMLInputElement>) {
+    let value = parseInt(e.target.value, 10);
+    if (value) {
+      setOwnerId(value);
+    } else {
+      setOwnerId(null);
+    }
+  }
+
   async function onSubmit(e: FormEvent) {
     e.preventDefault();
-    console.log('submitting', code, credit, date, type);
+    console.log('submitting', code, credit, date, type, ownerId);
     try {
       await axios.post('/api/coupons', {
         code,
         credit,
         expiresAt: date,
         type: type.value,
+        ownerId,
       });
       setCode('');
     } catch (err) {
@@ -77,6 +88,7 @@ export default function CouponIssueSection({}) {
         ]} value={type} onChange={onChangeType} /></Box>
         <Box><Label>쿠폰코드</Label><Input onChange={onChangeCode} type="text" placeholder="쿠폰 코드" required value={code}/></Box>
         <Box><Label>크레딧</Label><Input onChange={onChangeCredit} type="number" placeholder="크레딧 수" value={credit} /></Box>
+        <Box><Label>사용자</Label><Input onChange={onChangeOwnerId} type="number" placeholder="지급 대상(빈칸이면 누구나 가능)" value={ownerId} /></Box>
         <Box><Label>만료기한</Label><DatePicker onChange={onChangeDate} propertyType="date" value={date?.toString()} /></Box>
         <Box><Button variant="contained" onClick={onSubmit}>생성</Button></Box>
       </Card>
