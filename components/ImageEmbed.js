1e3552041b3fd1c59499afa7a5e96ebfcbd88751
zerocho <zerohch0@gmail.com>
2024-08-10 21:09:59 +0900

    add: delta E sort

--- components/ImageEmbed.js
+++ components/ImageEmbed.js
@@ -71,40 +71,35 @@ export const ImageEmbed = () => {
             });
         }
     };
-    function rgb2hsv(r, g, b) {
-        let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
-        rabs = r / 255;
-        gabs = g / 255;
-        babs = b / 255;
-        v = Math.max(rabs, gabs, babs),
-            diff = v - Math.min(rabs, gabs, babs);
-        diffc = (c) => (v - c) / 6 / diff + 1 / 2;
-        percentRoundFn = (num) => Math.round(num * 100) / 100;
-        if (diff == 0) {
-            h = s = 0;
-        }
-        else {
-            s = diff / v;
-            rr = diffc(rabs);
-            gg = diffc(gabs);
-            bb = diffc(babs);
-            if (rabs === v) {
-                h = bb - gg;
-            }
-            else if (gabs === v) {
-                h = (1 / 3) + rr - bb;
-            }
-            else if (babs === v) {
-                h = (2 / 3) + gg - rr;
-            }
-            if (h < 0) {
-                h += 1;
-            }
-            else if (h > 1) {
-                h -= 1;
-            }
-        }
-        return [Math.round(h * 360), percentRoundFn(s * 100), percentRoundFn(v * 100)];
+    function rgb2lab(rgb) {
+        let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
+        r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+        g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+        b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+        x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+        y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+        z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+        x = (x > 0.008856) ? Math.pow(x, 1 / 3) : (7.787 * x) + 16 / 116;
+        y = (y > 0.008856) ? Math.pow(y, 1 / 3) : (7.787 * y) + 16 / 116;
+        z = (z > 0.008856) ? Math.pow(z, 1 / 3) : (7.787 * z) + 16 / 116;
+        return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)];
+    }
+    function deltaE(labA, labB) {
+        let deltaL = labA[0] - labB[0];
+        let deltaA = labA[1] - labB[1];
+        let deltaB = labA[2] - labB[2];
+        let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+        let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+        let deltaC = c1 - c2;
+        let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+        deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+        let sc = 1.0 + 0.045 * c1;
+        let sh = 1.0 + 0.015 * c1;
+        let deltaLKlsl = deltaL / (1.0);
+        let deltaCkcsc = deltaC / (sc);
+        let deltaHkhsh = deltaH / (sh);
+        let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+        return i < 0 ? 0 : Math.sqrt(i);
     }
     const onColorStart = async () => {
         const response2 = await axios.get(`/api/collections/noColors`);
@@ -119,10 +114,10 @@ export const ImageEmbed = () => {
                     console.log(result);
                     let i = 0;
                     for (const rr of result.slice(0, 4)) {
-                        const hsv = rgb2hsv(...rr);
-                        console.log('hsv', hsv, rr);
+                        const lab = rgb2lab(rr);
+                        console.log('lab', lab, rr);
                         await axios.post(`/api/collections/${r.imageId}/colors`, {
-                            hsv: hsv.join(','),
+                            hsv: lab.join(','),
                             dominant: i === 0,
                         });
                         i++;
@@ -174,8 +169,14 @@ export const ImageEmbed = () => {
         setResult(result);
     };
     const onColorClick = async () => {
-        const response = await axios.get(`/api/collections/${color}/similarColors`);
-        setResult(response.data);
+        const rgb = color.split(',').map((v) => parseFloat(v));
+        const lab = rgb2lab(rgb);
+        const response = await axios.get(`/api/collections/allColors`);
+        response.data.forEach((color) => {
+            const lab2 = color.color.split(',').map((v) => parseFloat(v.replace(/[()]/g, '')));
+            color.delta = deltaE(lab, lab2);
+        });
+        setResult(response.data.toSorted((a, b) => a.delta - b.delta));
     };
     return (React.createElement("div", null,
         React.createElement("img", { id: 'image', alt: '', width: 256, height: 256 }),
--- components/ImageEmbed.tsx
+++ components/ImageEmbed.tsx
@@ -84,37 +84,43 @@ export const ImageEmbed: React.FC = () => {
     }
   };
 
-  function rgb2hsv (r: number, g: number, b: number) {
-    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
-    rabs = r / 255;
-    gabs = g / 255;
-    babs = b / 255;
-    v = Math.max(rabs, gabs, babs),
-      diff = v - Math.min(rabs, gabs, babs);
-    diffc = (c: number) => (v - c) / 6 / diff + 1 / 2;
-    percentRoundFn = (num: number) => Math.round(num * 100) / 100;
-    if (diff == 0) {
-      h = s = 0;
-    } else {
-      s = diff / v;
-      rr = diffc(rabs);
-      gg = diffc(gabs);
-      bb = diffc(babs);
+  function rgb2lab(rgb: [number, number,number]){
+    let r = rgb[0] / 255,
+      g = rgb[1] / 255,
+      b = rgb[2] / 255,
+      x, y, z;
 
-      if (rabs === v) {
-        h = bb - gg;
-      } else if (gabs === v) {
-        h = (1 / 3) + rr - bb;
-      } else if (babs === v) {
-        h = (2 / 3) + gg - rr;
-      }
-      if (h < 0) {
-        h += 1;
-      }else if (h > 1) {
-        h -= 1;
-      }
-    }
-    return [Math.round(h * 360), percentRoundFn(s * 100), percentRoundFn(v * 100)] as const;
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+    x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+    y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+    z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)] as const;
+  }
+
+  function deltaE(labA: readonly [number, number, number], labB: readonly [number, number, number]){
+    let deltaL = labA[0] - labB[0];
+    let deltaA = labA[1] - labB[1];
+    let deltaB = labA[2] - labB[2];
+    let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    let deltaC = c1 - c2;
+    let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    let sc = 1.0 + 0.045 * c1;
+    let sh = 1.0 + 0.015 * c1;
+    let deltaLKlsl = deltaL / (1.0);
+    let deltaCkcsc = deltaC / (sc);
+    let deltaHkhsh = deltaH / (sh);
+    let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+    return i < 0 ? 0 : Math.sqrt(i);
   }
 
   const onColorStart = async () => {
@@ -130,10 +136,10 @@ export const ImageEmbed: React.FC = () => {
           console.log(result);
           let i = 0;
           for (const rr of result.slice(0, 4)) {
-            const hsv = rgb2hsv(...rr);
-            console.log('hsv', hsv, rr);
+            const lab = rgb2lab(rr);
+            console.log('lab', lab, rr);
             await axios.post(`/api/collections/${r.imageId}/colors`, {
-              hsv: hsv.join(','),
+              hsv: lab.join(','),
               dominant: i === 0,
             })
             i++;
@@ -196,8 +202,16 @@ export const ImageEmbed: React.FC = () => {
   }
 
   const onColorClick = async () => {
-    const response = await axios.get<{ imageId: number; }[]>(`/api/collections/${color}/similarColors`);
-    setResult(response.data);
+    // const response = await axios.get<{ imageId: number; }[]>(`/api/collections/${color}/similarColors`);
+    // setResult(response.data);
+    const rgb = color.split(',').map((v) => parseFloat(v)) as [number, number, number];
+    const lab = rgb2lab(rgb);
+    const response = await axios.get<{ imageId: number; color: string; delta?: number }[]>(`/api/collections/allColors`);
+    response.data.forEach((color) => {
+      const lab2 = color.color.split(',').map((v) => parseFloat(v.replace(/[()]/g, ''))) as [number, number, number]
+      color.delta = deltaE(lab, lab2);
+    });
+    setResult(response.data.toSorted((a, b) => a.delta - b.delta));
   };
 
   return (
