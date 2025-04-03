948fc857850a1ae8613ab62a7dc62190e4f48954
zerocho <zerohch0@gmail.com>
2025-04-03 09:03:07 +0900

    update: covertParam

--- convertParam.js
+++ convertParam.js
@@ -1,50 +1,46 @@
 export const isNumeric = (value) => {
-    const stringValue = (String(value)).replace(/,/g, '.');
-    if (isNaN(parseFloat(stringValue)))
-        return false;
-    return isFinite(Number(stringValue));
+  const stringValue = String(value).replace(/,/g, ".");
+  if (isNaN(parseFloat(stringValue))) return false;
+  return isFinite(Number(stringValue));
 };
 export const safeParseNumber = (value) => {
-    if (isNumeric(value))
-        return Number(value);
-    return value;
+  if (isNumeric(value)) return Number(value);
+  return value;
 };
 export const convertParam = (property, fields, value, nested = false) => {
-    const type = property.type();
-    if (type === 'mixed')
-        return value;
-    if (type === 'number' && (typeof value === 'string' || typeof value === 'number')) {
-        return safeParseNumber(value);
+  const type = property.type();
+  if (type === "mixed") return value;
+  if (
+    type === "number" &&
+    (typeof value === "string" || typeof value === "number")
+  ) {
+    return safeParseNumber(value);
+  }
+  if (type === "boolean") {
+    return value === true || value === "true";
+  }
+  if (type === "reference") {
+    const foreignColumn = fields.find(
+      (field) => field.name === property.foreignColumnName()
+    );
+    if (!foreignColumn) {
+      return undefined;
     }
-    if (type === 'boolean') {
-        return value === true || value === 'true';
+    if (value === undefined || value === null) return value;
+    const foreignColumnType = foreignColumn.type;
+    if (nested) {
+      if (value === undefined || value === null) return { disconnect: true };
+      if (foreignColumnType === "String")
+        return { connect: { id: String(value) } };
+      if (typeof value === "string" || typeof value === "number") {
+        return { connect: { id: safeParseNumber(value) } };
+      }
     }
-    if (type === 'reference') {
-        const foreignColumn = fields.find((field) => field.name === property.foreignColumnName());
-        if (!foreignColumn) {
-            return undefined;
-        }
-        if (value === undefined || value === null)
-            return value;
-        const foreignColumnType = foreignColumn.type;
-        if (nested) {
-            if (value === undefined || value === null)
-                return { disconnect: true };
-            if (foreignColumnType === 'String')
-                return { connect: { id: String(value) } };
-            if (typeof value === 'string' || typeof value === 'number') {
-                return { connect: { id: safeParseNumber(value) } };
-            }
-        }
-        if (foreignColumnType === 'String')
-            return String(value);
-        if (typeof value === 'string' || typeof value === 'number') {
-            return safeParseNumber(value);
-        }
-        if (typeof value === 'object' && foreignColumn.name === 'collectionInfoId') {
-            return parseInt(value.Original, 10);
-        }
+    if (foreignColumnType === "String") return String(value);
+    if (typeof value === "string" || typeof value === "number") {
+      return safeParseNumber(value);
     }
-    return value;
+  }
+  return value;
 };
 //# sourceMappingURL=convertParam.js.map
--- convertParam.ts
+++ convertParam.ts
@@ -1,51 +1,53 @@
-import {Property} from "./customProperty.js";
+import { Property } from "./customProperty.js";
 
 export const isNumeric = (value: string | number): value is string => {
-  const stringValue = (String(value)).replace(/,/g, '.');
-  if (isNaN(parseFloat(stringValue)))
-    return false;
+  const stringValue = String(value).replace(/,/g, ".");
+  if (isNaN(parseFloat(stringValue))) return false;
   return isFinite(Number(stringValue));
 };
 export const safeParseNumber = (value: string | number) => {
-  if (isNumeric(value))
-    return Number(value);
+  if (isNumeric(value)) return Number(value);
   return value;
 };
 
-export const convertParam = (property: Property, fields: readonly { name: string, type: string }[], value: unknown, nested = false) => {
+export const convertParam = (
+  property: Property,
+  fields: readonly { name: string; type: string }[],
+  value: unknown,
+  nested = false
+) => {
   const type = property.type();
-  if (type === 'mixed')
-    return value;
-  if (type === 'number' && (typeof value === 'string' || typeof value === 'number')) {
+  if (type === "mixed") return value;
+  if (
+    type === "number" &&
+    (typeof value === "string" || typeof value === "number")
+  ) {
     return safeParseNumber(value);
   }
-  if (type === 'boolean') {
-    return value === true || value === 'true'
+  if (type === "boolean") {
+    return value === true || value === "true";
   }
-  if (type === 'reference') {
-    const foreignColumn = fields.find((field) => field.name === property.foreignColumnName());
+  if (type === "reference") {
+    const foreignColumn = fields.find(
+      (field) => field.name === property.foreignColumnName()
+    );
     if (!foreignColumn) {
       return undefined;
     }
-    if (value === undefined || value === null)
-      return value;
+    if (value === undefined || value === null) return value;
     const foreignColumnType = foreignColumn.type;
-    if (nested) { // 중첩된 모델인 경우
-      if (value === undefined || value === null)
-        return { disconnect: true };
-      if (foreignColumnType === 'String')
+    if (nested) {
+      // 중첩된 모델인 경우
+      if (value === undefined || value === null) return { disconnect: true };
+      if (foreignColumnType === "String")
         return { connect: { id: String(value) } };
-      if (typeof value === 'string' || typeof value === 'number') {
+      if (typeof value === "string" || typeof value === "number") {
         return { connect: { id: safeParseNumber(value) } };
       }
     }
-    if (foreignColumnType === 'String')
-      return String(value);
-    if (typeof value === 'string' || typeof value === 'number') {
+    if (foreignColumnType === "String") return String(value);
+    if (typeof value === "string" || typeof value === "number") {
       return safeParseNumber(value);
-    }
-    if (typeof value === 'object' && foreignColumn.name === 'collectionInfoId') {
-      return parseInt((value as Record<string, any>).Original, 10);
     }
   }
   return value;
