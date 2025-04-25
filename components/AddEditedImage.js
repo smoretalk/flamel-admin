f1fcd130fd8db1836875427dce26557eeeadb868
zerocho <zerohch0@gmail.com>
2025-04-25 12:17:42 +0900

    update: 편집 이미지 업로드 시 배경 제거 여부 체크

--- components/AddEditedImage.js
+++ components/AddEditedImage.js
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
-import { Button, FormGroup, Label } from "@adminjs/design-system";
+import { Button, FormGroup, Label, CheckBox } from "@adminjs/design-system";
 import axios from "axios";
 export default function AddEditedImage(props) {
     const { record } = props;
     const [file, setFile] = useState(null);
+    const [isBackgroundRemoved, setIsBackgroundRemoved] = useState(false);
     const handleChange = (e) => {
         setFile(e.target.files[0]);
     };
     const onCopy = (e) => {
         e.preventDefault();
         if (!file) {
-            return alert('파일을 업로드하세요.');
+            return alert("파일을 업로드하세요.");
         }
         const formData = new FormData();
-        formData.append('file', file);
-        axios.post(`/api/images/${record.id}/editedImage`, formData)
+        formData.append("bgRemoved", isBackgroundRemoved.toString());
+        formData.append("file", file);
+        axios
+            .post(`/api/images/${record.id}/editedImage`, formData)
             .then((response) => {
-            alert('편집 이미지를 추가했습니다.');
+            alert("편집 이미지를 추가했습니다.");
             location.href = `/admin/resources/Image/records/${response.data.imageId}/show`;
         })
             .catch(console.error);
     };
     return (React.createElement(FormGroup, null,
-        React.createElement(Label, null, "\uD3B8\uC9D1 \uC774\uBBF8\uC9C0 \uCD94\uAC00"),
-        React.createElement("input", { type: "file", accept: "image/*", onChange: handleChange }),
-        React.createElement(Button, { onClick: onCopy }, "\uD655\uC778")));
+        React.createElement("div", null,
+            React.createElement(Label, null, "\uD3B8\uC9D1 \uC774\uBBF8\uC9C0 \uCD94\uAC00"),
+            React.createElement("input", { type: "file", accept: "image/*", onChange: handleChange })),
+        React.createElement("div", null,
+            React.createElement(Label, null, "\uBC30\uACBD \uC81C\uAC70\uB41C \uC774\uBBF8\uC9C0?"),
+            React.createElement(CheckBox, { checked: isBackgroundRemoved, onChange: () => {
+                    setIsBackgroundRemoved(!isBackgroundRemoved);
+                } })),
+        React.createElement(Button, { onClick: onCopy }, "\uCD94\uAC00\uD558\uAE30")));
 }
 //# sourceMappingURL=AddEditedImage.js.map
--- components/AddEditedImage.tsx
+++ components/AddEditedImage.tsx
@@ -1,36 +1,50 @@
-import React, {ChangeEventHandler, useState} from "react";
-import {Button, FormGroup, Label } from "@adminjs/design-system";
-import {EditPropertyProps} from "adminjs";
+import React, { ChangeEventHandler, useState } from "react";
+import { Button, FormGroup, Label, CheckBox } from "@adminjs/design-system";
+import { EditPropertyProps } from "adminjs";
 import axios from "axios";
 
 export default function AddEditedImage(props: EditPropertyProps) {
-  const {record} = props
+  const { record } = props;
   const [file, setFile] = useState<File>(null);
+  const [isBackgroundRemoved, setIsBackgroundRemoved] = useState(false);
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e): void => {
     setFile(e.target.files[0]);
-  }
+  };
 
   const onCopy = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (!file) {
-      return alert('파일을 업로드하세요.');
+      return alert("파일을 업로드하세요.");
     }
     const formData = new FormData();
-    formData.append('file', file);
-    axios.post(`/api/images/${record.id}/editedImage`, formData)
+    formData.append("bgRemoved", isBackgroundRemoved.toString());
+    formData.append("file", file);
+    axios
+      .post(`/api/images/${record.id}/editedImage`, formData)
       .then((response) => {
-        alert('편집 이미지를 추가했습니다.');
+        alert("편집 이미지를 추가했습니다.");
         location.href = `/admin/resources/Image/records/${response.data.imageId}/show`;
       })
       .catch(console.error);
-  }
+  };
 
   return (
     <FormGroup>
-      <Label>편집 이미지 추가</Label>
-      <input type="file" accept="image/*" onChange={handleChange}/>
-      <Button onClick={onCopy}>확인</Button>
+      <div>
+        <Label>편집 이미지 추가</Label>
+        <input type="file" accept="image/*" onChange={handleChange} />
+      </div>
+      <div>
+        <Label>배경 제거된 이미지?</Label>
+        <CheckBox
+          checked={isBackgroundRemoved}
+          onChange={() => {
+            setIsBackgroundRemoved(!isBackgroundRemoved);
+          }}
+        />
+      </div>
+      <Button onClick={onCopy}>추가하기</Button>
     </FormGroup>
-  )
+  );
 }
