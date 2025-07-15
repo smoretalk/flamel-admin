f00530010343450ff3546667e66585b950b6a375
zerocho <zerohch0@gmail.com>
2025-07-15 17:10:04 +0900

    update: pro coupon

--- components/CouponIssueSection.js
+++ components/CouponIssueSection.js
@@ -5,12 +5,15 @@ import { useTranslation } from "adminjs";
 import axios from "axios";
 export default function CouponIssueSection({}) {
     const { translateMessage } = useTranslation();
-    const [type, setType] = useState({ value: 'defaultCredit', label: '디폴트 크레딧' });
+    const [type, setType] = useState({
+        value: "defaultCredit",
+        label: "디폴트 크레딧",
+    });
     const [date, setDate] = useState(null);
-    const [email, setEmail] = useState('');
+    const [email, setEmail] = useState("");
     const [maxCount, setMaxCount] = useState(null);
-    const [code, setCode] = useState('');
-    const [reason, setReason] = useState('');
+    const [code, setCode] = useState("");
+    const [reason, setReason] = useState("");
     const [credit, setCredit] = useState(0);
     const [sendEmail, setSendEmail] = useState(false);
     function onChangeDate(date) {
@@ -39,9 +42,9 @@ export default function CouponIssueSection({}) {
     }
     async function onSubmit(e) {
         e.preventDefault();
-        console.log('submitting', code, credit, date, type, maxCount, email, reason, sendEmail);
+        console.log("submitting", code, credit, date, type, maxCount, email, reason, sendEmail);
         try {
-            await axios.post('/api/coupons', {
+            await axios.post("/api/coupons", {
                 code,
                 credit,
                 expiresAt: date,
@@ -51,7 +54,7 @@ export default function CouponIssueSection({}) {
                 reason,
                 sendEmail,
             });
-            setCode('');
+            setCode("");
             alert("쿠폰이 발급되었습니다!");
         }
         catch (err) {
@@ -71,37 +74,39 @@ export default function CouponIssueSection({}) {
     return (React.createElement(Box, { width: [1, 1, 1 / 2], p: "lg" },
         React.createElement(Card, { as: "form", onSubmit: onSubmit },
             React.createElement(Icon, { icon: "Gift" }),
-            React.createElement(H5, { mt: "lg" }, translateMessage('issueCoupon_title')),
+            React.createElement(H5, { mt: "lg" }, translateMessage("issueCoupon_title")),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uC720\uD615"),
                 React.createElement(Select, { options: [
-                        { value: 'defaultCredit', label: '디폴트 크레딧' },
-                        { value: 'dailyCredit', label: '일간 크레딧' },
-                        { value: 'monthlyCredit', label: '월간 크레딧' },
-                        { value: 'onetime', label: '일일권' },
-                        { value: 'basic', label: 'basic 요금제' },
+                        { value: "defaultCredit", label: "디폴트 크레딧" },
+                        { value: "dailyCredit", label: "일간 크레딧" },
+                        { value: "monthlyCredit", label: "월간 크레딧" },
+                        { value: "onetime", label: "일일권" },
+                        { value: "basic", label: "basic 요금제" },
+                        { value: "pro", label: "pro 요금제" },
                     ], value: type, onChange: onChangeType })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uCFE0\uD3F0\uCF54\uB4DC"),
-                React.createElement(Input, { onChange: onChangeCode, type: "text", placeholder: "\uCFE0\uD3F0 \uCF54\uB4DC", required: true, value: code, style: { width: '100%' } })),
+                React.createElement(Input, { onChange: onChangeCode, type: "text", placeholder: "\uCFE0\uD3F0 \uCF54\uB4DC", required: true, value: code, style: { width: "100%" } })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uD06C\uB808\uB527or\uAE30\uAC04"),
-                React.createElement(Input, { onChange: onChangeCredit, type: "number", placeholder: "\uD06C\uB808\uB527 \uC218 or \uAE30\uAC04(basic)", value: credit, style: { width: '100%' } })),
+                React.createElement(Input, { onChange: onChangeCredit, type: "number", placeholder: "\uD06C\uB808\uB527 \uC218 or \uAE30\uAC04(basic)", value: credit, style: { width: "100%" } })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uBC1C\uD589\uB7C9"),
-                React.createElement(Input, { onChange: onChangeMaxCount, type: "number", placeholder: "\uBC1C\uD589\uB7C9(\uBE44\uC6CC\uB450\uBA74 1)", value: maxCount, style: { width: '100%' } })),
+                React.createElement(Input, { onChange: onChangeMaxCount, type: "number", placeholder: "\uBC1C\uD589\uB7C9(\uBE44\uC6CC\uB450\uBA74 1)", value: maxCount, style: { width: "100%" } })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uC0AC\uC6A9\uC790"),
-                React.createElement(Input, { onChange: onChangeEmail, placeholder: "\uC9C0\uAE09 \uB300\uC0C1 \uC774\uBA54\uC77C(\uBE48\uCE78\uC774\uBA74 \uB204\uAD6C\uB098 \uAC00\uB2A5)", value: email, style: { width: '100%' } })),
+                React.createElement(Input, { onChange: onChangeEmail, placeholder: "\uC9C0\uAE09 \uB300\uC0C1 \uC774\uBA54\uC77C(\uBE48\uCE78\uC774\uBA74 \uB204\uAD6C\uB098 \uAC00\uB2A5)", value: email, style: { width: "100%" } })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uB9CC\uB8CC\uAE30\uD55C"),
                 React.createElement(DatePicker, { onChange: onChangeDate, propertyType: "date", value: date?.toString() })),
             React.createElement(Box, null,
                 React.createElement(Label, null, "\uCFE0\uD3F0 \uBC1C\uAE09 \uC0AC\uC720(HTML)"),
-                React.createElement(Input, { onChange: onChangeReason, type: "text", placeholder: "\uCFE0\uD3F0 \uBC1C\uAE09 \uC0AC\uC720", required: true, value: reason, style: { width: '100%' } })),
+                React.createElement(Input, { onChange: onChangeReason, type: "text", placeholder: "\uCFE0\uD3F0 \uBC1C\uAE09 \uC0AC\uC720", required: true, value: reason, style: { width: "100%" } })),
             React.createElement(Box, null,
                 React.createElement(Label, null,
-                    "\uC774\uBA54\uC77C \uC804\uC1A1 \uC5EC\uBD80 ",
+                    "\uC774\uBA54\uC77C \uC804\uC1A1 \uC5EC\uBD80",
+                    " ",
                     React.createElement(CheckBox, { onChange: onChangeSendEmail, checked: sendEmail }))),
             React.createElement(Box, null,
                 React.createElement(Button, { variant: "contained", onClick: onSubmit }, "\uC0DD\uC131")))));
--- components/CouponIssueSection.tsx
+++ components/CouponIssueSection.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
-  Button, CheckBox,
+  Button,
+  CheckBox,
   DatePicker,
   H5,
   Icon,
@@ -8,19 +9,22 @@ import {
   Label,
   Select,
 } from "@adminjs/design-system";
-import React, {ChangeEvent, FormEvent, useState} from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Card from "./Card.js";
-import {useTranslation} from "adminjs";
+import { useTranslation } from "adminjs";
 import axios from "axios";
 
 export default function CouponIssueSection({}) {
   const { translateMessage } = useTranslation();
-  const [type, setType] = useState<{ value: string; label: string }>({ value: 'defaultCredit', label: '디폴트 크레딧'});
+  const [type, setType] = useState<{ value: string; label: string }>({
+    value: "defaultCredit",
+    label: "디폴트 크레딧",
+  });
   const [date, setDate] = useState<Date | null>(null);
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [maxCount, setMaxCount] = useState<number | null>(null);
-  const [code, setCode] = useState<string>('');
-  const [reason, setReason] = useState<string>('');
+  const [code, setCode] = useState<string>("");
+  const [reason, setReason] = useState<string>("");
   const [credit, setCredit] = useState<number>(0);
   const [sendEmail, setSendEmail] = useState(false);
 
@@ -55,9 +59,19 @@ export default function CouponIssueSection({}) {
 
   async function onSubmit(e: FormEvent) {
     e.preventDefault();
-    console.log('submitting', code, credit, date, type, maxCount, email, reason, sendEmail);
+    console.log(
+      "submitting",
+      code,
+      credit,
+      date,
+      type,
+      maxCount,
+      email,
+      reason,
+      sendEmail
+    );
     try {
-      await axios.post('/api/coupons', {
+      await axios.post("/api/coupons", {
         code,
         credit,
         expiresAt: date,
@@ -67,7 +81,7 @@ export default function CouponIssueSection({}) {
         reason,
         sendEmail,
       });
-      setCode('');
+      setCode("");
       alert("쿠폰이 발급되었습니다!");
     } catch (err) {
       console.error(err);
@@ -90,23 +104,93 @@ export default function CouponIssueSection({}) {
     <Box width={[1, 1, 1 / 2]} p="lg">
       <Card as="form" onSubmit={onSubmit}>
         <Icon icon="Gift" />
-        <H5 mt="lg">{translateMessage('issueCoupon_title')}</H5>
-        <Box><Label>유형</Label><Select options={[
-          { value: 'defaultCredit', label: '디폴트 크레딧' },
-          { value: 'dailyCredit', label: '일간 크레딧' },
-          { value: 'monthlyCredit', label: '월간 크레딧' },
-          { value: 'onetime', label: '일일권' },
-          { value: 'basic', label: 'basic 요금제' },
-        ]} value={type} onChange={onChangeType} /></Box>
-        <Box><Label>쿠폰코드</Label><Input onChange={onChangeCode} type="text" placeholder="쿠폰 코드" required value={code} style={{width: '100%'}} /></Box>
-        <Box><Label>크레딧or기간</Label><Input onChange={onChangeCredit} type="number" placeholder="크레딧 수 or 기간(basic)" value={credit} style={{width: '100%'}} /></Box>
-        <Box><Label>발행량</Label><Input onChange={onChangeMaxCount} type="number" placeholder="발행량(비워두면 1)" value={maxCount} style={{width: '100%'}} /></Box>
-        <Box><Label>사용자</Label><Input onChange={onChangeEmail} placeholder="지급 대상 이메일(빈칸이면 누구나 가능)" value={email} style={{width: '100%'}} /></Box>
-        <Box><Label>만료기한</Label><DatePicker onChange={onChangeDate} propertyType="date" value={date?.toString()} /></Box>
-        <Box><Label>쿠폰 발급 사유(HTML)</Label><Input onChange={onChangeReason} type="text" placeholder="쿠폰 발급 사유" required value={reason} style={{width: '100%'}} /></Box>
-        <Box><Label>이메일 전송 여부 <CheckBox onChange={onChangeSendEmail} checked={sendEmail} /></Label></Box>
-        <Box><Button variant="contained" onClick={onSubmit}>생성</Button></Box>
+        <H5 mt="lg">{translateMessage("issueCoupon_title")}</H5>
+        <Box>
+          <Label>유형</Label>
+          <Select
+            options={[
+              { value: "defaultCredit", label: "디폴트 크레딧" },
+              { value: "dailyCredit", label: "일간 크레딧" },
+              { value: "monthlyCredit", label: "월간 크레딧" },
+              { value: "onetime", label: "일일권" },
+              { value: "basic", label: "basic 요금제" },
+              { value: "pro", label: "pro 요금제" },
+            ]}
+            value={type}
+            onChange={onChangeType}
+          />
+        </Box>
+        <Box>
+          <Label>쿠폰코드</Label>
+          <Input
+            onChange={onChangeCode}
+            type="text"
+            placeholder="쿠폰 코드"
+            required
+            value={code}
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <Label>크레딧or기간</Label>
+          <Input
+            onChange={onChangeCredit}
+            type="number"
+            placeholder="크레딧 수 or 기간(basic)"
+            value={credit}
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <Label>발행량</Label>
+          <Input
+            onChange={onChangeMaxCount}
+            type="number"
+            placeholder="발행량(비워두면 1)"
+            value={maxCount}
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <Label>사용자</Label>
+          <Input
+            onChange={onChangeEmail}
+            placeholder="지급 대상 이메일(빈칸이면 누구나 가능)"
+            value={email}
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <Label>만료기한</Label>
+          <DatePicker
+            onChange={onChangeDate}
+            propertyType="date"
+            value={date?.toString()}
+          />
+        </Box>
+        <Box>
+          <Label>쿠폰 발급 사유(HTML)</Label>
+          <Input
+            onChange={onChangeReason}
+            type="text"
+            placeholder="쿠폰 발급 사유"
+            required
+            value={reason}
+            style={{ width: "100%" }}
+          />
+        </Box>
+        <Box>
+          <Label>
+            이메일 전송 여부{" "}
+            <CheckBox onChange={onChangeSendEmail} checked={sendEmail} />
+          </Label>
+        </Box>
+        <Box>
+          <Button variant="contained" onClick={onSubmit}>
+            생성
+          </Button>
+        </Box>
       </Card>
     </Box>
-  )
+  );
 }
